07911f088f3ada165323c7adc153e546cb574cf5
ItsAlwys2LaTE <139423262+ItsAlwys2LaTE@users.noreply.github.com>
2024-11-25 11:53:29 -0800

    Delete fooDelivery directory

--- fooDelivery/backend/config/db.js
+++
@@ -1,7 +0,0 @@
-import mongoose from "mongoose";
-
-export const  connectDB = async () =>{
-
-    await mongoose.connect('mongodb+srv://anupamsharma12102004:Anupam1210@cluster0.wt737.mongodb.net/fooDelivery').then(()=>console.log("DB Connected"));
-   
-}
